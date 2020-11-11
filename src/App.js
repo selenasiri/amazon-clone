77f8a025cd40adac92b228faee218c83519e4259
Selena Siri <selenasiri@gmail.com>
2020-11-11 14:50:00 -0500

    basic header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // BEM convention - lowercase classNames
+    <div className="app">
+      <h1>Hello Clever Programmers, let's build the Amazon Store </h1>
+      {/* Header */}
+      <Header />
+      {/* Home */}
     </div>
   );
 }

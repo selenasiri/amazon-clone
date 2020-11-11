23aefa3c385cae65f9ae96ca42f09f60ebd42519
Selena Siri <selenasiri@gmail.com>
2020-11-11 14:16:17 -0500

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBuImX-Ydju7LKrVpLWAt5TDZvoTUduOc4",
+  authDomain: "challenge-yt.firebaseapp.com",
+  databaseURL: "https://challenge-yt.firebaseio.com",
+  projectId: "challenge-yt",
+  storageBucket: "challenge-yt.appspot.com",
+  messagingSenderId: "877102223097",
+  appId: "1:877102223097:web:e6a482ca80bf7d04089c28",
+  measurementId: "G-KPZZMCSP8Q"
+};

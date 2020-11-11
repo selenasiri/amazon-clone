e846632a2f2489aa10ddb80bbf729880dd33169c
Selena Siri <selenasiri@gmail.com>
2020-11-11 15:39:56 -0500

    banner

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     // BEM convention - lowercase classNames
     <div className="app">
-      <h1>Hello Clever Programmers, let's build the Amazon Store </h1>
-      {/* Header */}
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
   return (
     <div className='header'>
       <img 
         className='header_logo'
-        src='https://pngimg.com/uploads/amazon/amazon_PNG21.png' 
+        src='https://pngimg.com/uploads/amazon/amazon_PNG11.png' 
+        alt='logo'
       />
       <div className='header_search'>
           <input
             className='header_searchInput' type='text' />
-          {/* Logo */}
+          <SearchIcon
+            className='header_searchIcon' />
       </div>
       <div className='header_nav'>
         <div className='header_option'>
@@ -29,7 +33,10 @@ function Header() {
           <span className='header_optionLineTwo'>Prime</span>
         </div>
         
-
+        <div className='header_optionBasket'>
+          <ShoppingBasketIcon />
+          <span className='header_optionLineTwo header_basketCount'>0</span>
+        </div>
       </div>
     </div>
   )

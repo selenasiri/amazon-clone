5455b91fc5a0a3b0961f11fb45062ce4189af8ab
Selena Siri <selenasiri@gmail.com>
2020-11-12 15:10:40 -0500

    Proceed to Checkout

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Checkout from './Checkout';
 import Login from './Login';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider'
+import Payment from './Payment'
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -37,12 +38,16 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
-        <Route path='/login'>
+          <Route path='/login'>
             <Login />
           </Route>
           <Route path='/checkout'>
             <Header />
             <Checkout />
+          </Route>
+          <Route path='/payment'>
+            <Header />
+            <Payment />
           </Route>
           <Route path='/'>
             <Header />
---
+++ src/Payment.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import CheckoutProduct from './CheckoutProduct';
+import './Payment.css'
+import { useStateValue } from './StateProvider';
+import { Link } from 'react-router-dom'
+
+const Payment = () => {
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  return (
+    <div className='payment'>
+      <div className='payment_container'>
+        <h1>
+          Checkout (
+            <Link to='/checkout'>{basket?.length} items</Link>
+          )
+        </h1>
+
+        {/* Payment section - delivery address */}
+          <div className="payment_section">
+            <div className='payment_title'>
+              <h3>Delivery Address</h3>
+            </div>
+            <div className='payment_address'>
+              <p>{user?.email}</p>
+              <p>123 React Lane</p>
+              <p>Los Angeles, CA</p>
+            </div>
+          </div>
+
+        {/* Payment section - reviewing items */}
+          <div className="payment_section">
+            <div className='payment_title'>
+              <h3>Review items and delivery</h3>
+            </div>
+            <div className='payment_items'>
+              {/* Show all products here */}
+              {basket.map(item => (
+                <CheckoutProduct
+                  id={item.id}
+                  title={item.title}
+                  image={item.image}
+                  price={item.price}
+                  rating={item.rating}
+                />
+              ))}
+            </div>
+          </div>
+
+        {/* Payment section - payment method */}
+          <div className="payment_section">
+              <div className='payment_title'>
+                <h3>Payment Method</h3>
+                <div className='payment_details'>
+                  {/* Stripe magic will go */}
+                </div>
+              </div>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Payment
+

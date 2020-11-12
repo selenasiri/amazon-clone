9ddb21930d7f63d208a61c26b4554d8b23c2ed00
Selena Siri <selenasiri@gmail.com>
2020-11-12 13:38:22 -0500

    authentication

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,16 @@ import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
 
 function Header() {
-  const [{basket}, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut();
+    }
+  }
 
   return (
     <div className='header'>
@@ -25,10 +32,12 @@ function Header() {
             className='header_searchIcon' />
       </div>
       <div className='header_nav'>
-        <Link to='/login'>
-          <div className='header_option'>
+        <Link to={!user && '/login'}>
+          <div onClick={handleAuthentication} className='header_option'>
             <span className='header_optionLineOne'>Hello Guest</span> 
-            <span className='header_optionLineTwo'>Sign In</span>
+            <span className='header_optionLineTwo'>
+              {user ? 'Sign Out' : 'Sign In'}
+            </span>
           </div>
         </Link>
 

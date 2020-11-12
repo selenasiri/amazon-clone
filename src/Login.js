2a6416444a2422d8d164b33c6267849885d6eba3
Selena Siri <selenasiri@gmail.com>
2020-11-12 12:31:36 -0500

    login

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import { Link } from 'react-router-dom'
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = e => {
+    e.preventDefault();
+
+    // firebase magic...
+  }
+
+  const register = e => {
+    e.preventDefault();
+
+    // firebase magic...
+  }
+
   return (
     <div className='login'>
       <Link to='/'>
@@ -16,19 +31,19 @@ function Login() {
 
         <form>
           <h5>Email</h5>
-          <input type='text' />
-          
-          <h5>Password</h5>
-          <input type='password' />
+          <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
 
-          <button className='login_signInButton'>Sign In</button>
+          <h5>Password</h5>
+          <input type='password' value={password} onChange={e => setEmail(e.target.value)} />
+
+          <button type='submit' onClick={signIn} className='login_signInButton'>Sign In</button>
         </form>
 
         <p>
           By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Nitice, our Cookies Notice and our Interest-Based Ads Notice.
         </p>
 
-        <button className='login_registerButton'>Create your Amazon account</button> 
+        <button onClick={register} className='login_registerButton'>Create your Amazon account</button> 
       </div> 
     </div>
   )
